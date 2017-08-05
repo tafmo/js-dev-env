05031c258c0ff21ba74aeb16f45786f547cd13df
Gideon <daiquiri.ice@gmail.com>
2017-08-05 08:20:58 -0500

    finished 'HTTP Calls' lesson

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,6 +18,15 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  // Hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@example.com"},
+    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@example.com"},
+    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@example.com"}
+  ]);
+});
+
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
