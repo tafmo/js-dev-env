9264a29fdd101ff3472288787e123df801f589e0
Gideon <daiquiri.ice@gmail.com>
2017-08-09 20:51:25 -0500

    finished 'Production Build' lesson

---
+++ buildScripts/distServer.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import compression from 'compression';
+
+/* eslint-disable no-console */
+const port = 3000;
+const app = express();
+
+app.use(compression());
+app.use(express.static('dist'));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+app.get('/users', function(req, res) {
+  // Hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@example.com"},
+    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@example.com"},
+    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@example.com"}
+  ]);
+});
+
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open('http://localhost:' + port);
+  }
+});
